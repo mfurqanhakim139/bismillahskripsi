0d7f6a9d93bc359f984346113be07bf9c34935cf
mfurqanhakim <37473756+mfurqanhakim139@users.noreply.github.com>
2022-03-18 01:51:03 +0700

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -72,7 +72,7 @@ def parse(cls, api, raw):
         auth.set_access_token(atoken, asecret)
         api = tweepy.API(auth)
         twitter_stream = Stream(auth, MyListener(args.data_dir,args.query))
-twitter_stream.filter(track=["gempa", "#gempa"])
+        twitter_stream.filter(track=["gempa", "#gempa"])
 #auth = OAuthHandler(ckey, csecret)
 #auth.set_access_token(atoken, asecret)
 #twitterStream = Stream(auth, listener())
