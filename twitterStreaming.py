75ab295090428586d5d3306809344a18ebb11bbc
mfurqanhakim <37473756+mfurqanhakim139@users.noreply.github.com>
2022-03-18 01:49:02 +0700

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -29,7 +29,7 @@ class MyListener(StreamListener):
 def on_data(self, data):
     try:
         with open(self.outfile, 'a') as f:
-                  f.write(data)
+            f.write(data)
 print(data)
 return True
 except BaseException as e:
