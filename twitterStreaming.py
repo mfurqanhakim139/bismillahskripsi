eeecaee7b11c89fdfcbcc2d2c4ab050e1b8bdecc
mfurqanhakim <37473756+mfurqanhakim139@users.noreply.github.com>
2022-03-18 01:48:01 +0700

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -18,8 +18,7 @@ asecret = 'snYPccETaazso21XowfusBZKxcz8uQ2mxrjgoHYb8BIFv'
 def get_parser():
     """Get parser for command line arguments."""
     parser = argparse.ArgumentParser(description="Twitter Downloader")
-    parser.add_argument("-q",
-"--query",
+    parser.add_argument("-q","--query",
 dest="query",
 help="Query/Filter",
 default='-')
