073646e9522c08f2929fd555d29cd539a6cb10c3
mfurqanhakim <37473756+mfurqanhakim139@users.noreply.github.com>
2022-03-18 01:48:14 +0700

    Update twitterStreaming.py

--- twitterStreaming.py
+++ twitterStreaming.py
@@ -19,7 +19,7 @@ def get_parser():
     """Get parser for command line arguments."""
     parser = argparse.ArgumentParser(description="Twitter Downloader")
     parser.add_argument("-q","--query", dest="query", help="Query/Filter", default='-')
-parser.add_argument("-d",
+    parser.add_argument("-d",
 "--data-dir",
 dest="data_dir",
 help="Output/Data Directory")
